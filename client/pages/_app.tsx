f002d951bbd90c0d8c94f060cb48ce1522996383
Abhinav Bhattarai <69339222+Abhinav-Bhattarai@users.noreply.github.com>
2021-09-21 13:16:02 +0545

    FE: ApolloClient setup

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,15 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import useAuthorizationCheck from '../Hooks/useAuthorizationCheck';
-import LoadingPage from '../Components/UI/loadingPage';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import useAuthorizationCheck from "../Hooks/useAuthorizationCheck";
+import LoadingPage from "../Components/UI/loadingPage";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://localhost:8080/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const authStatus = useAuthorizationCheck();
 
   if (authStatus === null) {
-    return <LoadingPage/>
+    return <LoadingPage />;
   }
 
-  return <Component {...pageProps} authStatus={authStatus} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} authStatus={authStatus} />
+    </ApolloProvider>
+  );
 }
 export default MyApp;
