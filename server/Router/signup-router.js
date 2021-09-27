93619ebef31f4632a8180313c41c7005d2086b02
Abhinav Bhattarai <69339222+Abhinav-Bhattarai@users.noreply.github.com>
2021-09-27 11:25:46 +0545

    BE: UserRedundancy Handler

--- server/Router/signup-router.js
+++ server/Router/signup-router.js
@@ -11,23 +11,34 @@ const HashThisString = async (string) => {
   return hashedString;
 };
 
+export const CheckUserRedundency = async (Username) => {
+  const response = await UserModel.findOne({ Username });
+  if (response) return true;
+  return false;
+};
+
 router.post("/", SignupMiddleware, async (req, res) => {
   let { UserName, Password, Email } = req.body;
   Password = await HashThisString(Password);
-  const data = new UserModel({
-    Username: UserName,
-    Password,
-    Email,
-  });
-  const response = await data.save();
-  const config = { UserName, uid: response.uid, id: response._id };
-  const token = GenerateJWTToken(config);
-  if (token) {
-    res.setHeader("Set-Cookie", [
-      `authToken: ${token}; SameSite=Strict; httponly`,
-      `uid: ${AuthenticationStatus.uid}; httpOnly; SameSite=Strict`,
-    ]);
-    return res.json({ authStatus: true, error: false });
+  const UserRedundancyStatus = await CheckUserRedundency(UserName);
+  if (UserRedundancyStatus === false) {
+    const data = new UserModel({
+      Username: UserName,
+      Password,
+      Email,
+    });
+    const response = await data.save();
+    const config = { UserName, uid: response.uid, id: response._id };
+    const token = GenerateJWTToken(config);
+    if (token) {
+      res.setHeader("Set-Cookie", [
+        `authToken: ${token}; SameSite=Strict; httponly`,
+        `uid: ${AuthenticationStatus.uid}; httpOnly; SameSite=Strict`,
+      ]);
+      return res.json({ authStatus: true, error: false });
+    } else {
+      return res.json({ error: false, authStatus: false });
+    }
   } else {
     return res.json({ error: false, authStatus: false });
   }
